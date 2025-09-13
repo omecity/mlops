eefb01db38934012830fa36d9a6066852fab3c39
omecity <omeiza.olumoye@gmail.com>
2025-09-12 23:37:16 -0400

    read in the file and check the first few rows

--- Homework1/hw1.py
+++ Homework1/hw1.py
@@ -4,3 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
+
+
+# Read CSV file into pandas
+df = pd.read_csv("sampregdata.csv")
+
+
+
+# Take a look at the first few rows
+print("\nOriginal DataFrame: \n")
+print(df.head())
+print()
