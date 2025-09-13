cbfdffd64fef61bb08d4d623b6ecd7ee475751dd
omecity <omeiza.olumoye@gmail.com>
2025-09-12 23:34:40 -0400

    add required libraries

---
+++ Homework1/hw1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from itertools import combinations
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
